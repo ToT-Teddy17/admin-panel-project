8fe32754642269c4b6f8166e3c7357356507f0a8
Morning <morning@mstars-lab2-03s-iMac.local>
2023-01-31 13:06:21 +0800

    yanzlalt

---
+++ admin-panel-project/fe/src/pages/Product.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ProductFirstPage from "../components/ProductFirstPage";
+
+export default function Product() {
+  return (
+    <div>
+      <h1>Product First PAge</h1>
+      <ProductFirstPage />
+    </div>
+  );
+}
